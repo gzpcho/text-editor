9bfc604a1900db1b1afd393736b1147ab2bf128e
gzpcho <roquebertian@gmail.com>
2020-11-24 00:25:31 -0600

    Fixed tab rendering and added handler for special characters

--- text-editor.c
+++ text-editor.c
@@ -17,6 +17,7 @@
 /*** defines ***/
 
 #define CURR_VERSION "0.0.1"
+#define TAB_STOP 8
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -36,7 +37,9 @@ enum editorKey {
 
 typedef struct erow {
     int size;
+    int rsize;
     char *chars;
+    char *render;
 } erow;
 
 struct editorConfig {
@@ -167,6 +170,29 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** row operations ***/
 
+void editorUpdateRow(erow *row) {
+    int tabs = 0;
+    int j;
+    for(j = 0; j < row->size; j++) {
+        if(row->chars[j] == '\t') tabs++;
+    }
+
+    free(row->render);
+    row->render = malloc(row->size + tabs*(TAB_STOP - 1) + 1);
+
+    int idx = 0;
+    for(j = 0; j < row->size; j++) {
+        if(row->chars[j] == '\t') {
+            row->render[idx++] = ' ';
+            while(idx % TAB_STOP != 0) row->render[idx++] = ' ';
+        } else {
+            row->render[idx++] = row->chars[j];
+        }
+    }
+    row->render[idx] = '\0';
+    row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
     
@@ -175,6 +201,11 @@ void editorAppendRow(char *s, size_t len) {
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
     E.row[at].chars[len] = '\0';
+
+    E.row[at].rsize = 0;
+    E.row[at].render = NULL;
+    editorUpdateRow(&E.row[at]);
+
     E.numrows++;
 }
 
@@ -251,19 +282,16 @@ void editorDrawRows(struct abuf *ab) {
                     abAppend(ab, "~", 1);
                     padding--;
                 }
-                while (padding--) 
-                    abAppend(ab, " ", 1);
+                while (padding--) abAppend(ab, " ", 1);
                 abAppend(ab, welcome, welcomelen);
             } else {
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row[filerow].size - E.coloff;
-            if(len < 0)
-                len = 0;
-            if (len > E.screencols) 
-                len = E.screencols;
-            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+            int len = E.row[filerow].rsize - E.coloff;
+            if(len < 0) len = 0;
+            if (len > E.screencols) len = E.screencols;
+            abAppend(ab, &E.row[filerow].render[E.coloff], len);
         }
         
         abAppend(ab, "\x1b[K", 3);
